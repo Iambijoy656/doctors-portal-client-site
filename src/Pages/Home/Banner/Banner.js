351491796aca9bd0ddb55fafe0607d084a3e6393
Bijoy Chandro Das <freelancing.bijoy@gmail.com>
2022-12-03 01:17:50 +0600

    added logo

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -2,6 +2,7 @@ import React from 'react';
 import chair from '../../../assets/images/chair.png';
 import bg from '../../../assets/images/bg.png';
 import PrimaryButton from '../../../Components/PrimaryButton/PrimaryButton';
+import { Link } from 'react-router-dom';
 
 
 const Banner = () => {
@@ -13,10 +14,10 @@ const Banner = () => {
                 <div className="hero-content flex-col lg:flex-row-reverse">
                     <img src={chair} className=" lg:w-1/2 rounded-lg shadow-2xl" alt='' />
                     <div>
-                        <h1 className="text-5xl font-bold">Box Office News!</h1>
-                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                        <h1 className="text-5xl font-bold ">We are Achieve the Success of Treatment</h1>
+                        <p className="py-6 text-justify mr-5">Helping patients achieve good dental health & beautiful smile is a privilege & responsibility. For over 10 years, my team & I have proudly provided the best dental experience in Bangladesh. Our comfort-first approach is designed to meet the needs of you & your entire family.</p>
 
-                        <PrimaryButton>Get Started</PrimaryButton>
+                        <Link to='/appointment'> <PrimaryButton>Book An Appointment</PrimaryButton></Link>
 
                     </div>
                 </div>
